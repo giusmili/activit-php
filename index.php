8596b603fc44c6e26a045ead8cd5fefccce55598
giusmili <giusmili67@gmail.com>
2024-05-10 11:20:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include_once(__DIR__.'/src/head.inc.php');
+   
+?>
+<body>
+    <!-- https://codepen.io/gius-mili/pen/ZEoJGrB -->
+    <header>
+        <h1>Formulaire de contact</h1>
+    </header>
+    <main>
+        
+        <?php
+             include_once(__DIR__.'/src/form_control.inc.php');
+        ?>
+    </main>
+    <footer>
+        <p>
+            <?= Config::$_footer." ".$_date; ?>
+        </p>
+    </footer>
+    
+   
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 🚀Cours PHP
+
+## 💻Activité intégration et analyse formulaire 
+
+:link:[Source code](https://codepen.io/gius-mili/pen/xxjLGYP) &raquo;
